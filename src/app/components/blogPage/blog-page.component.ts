0c2538b91c7ea9c00f80a4c000c4ca1f563e3098
ArbazCh <72260630+ArbazCh@users.noreply.github.com>
2023-06-06 00:26:55 +0500

    blog comments

--- src/app/components/blogPage/blog-page.component.ts
+++ src/app/components/blogPage/blog-page.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from "@angular/core";
 import { BlogService } from "src/app/services/blog.service";
 import { ActivatedRoute, Router } from '@angular/router'
 import { IBlog } from "../my-blogs/blog";
+import { AuthService } from "src/app/services/auth.service";
 // import { getHttpOptions } from "src/app/helper/helper";
 
 @Component({
@@ -16,21 +17,35 @@ import { IBlog } from "../my-blogs/blog";
 
 export class BlogPageComponent implements OnInit {
 
-    constructor(private blogService: BlogService, private route: ActivatedRoute, private router: Router) { }
+    constructor(
+        private blogService: BlogService, 
+        private route: ActivatedRoute, 
+        private router: Router,
+        ) { }
 
     content!:string;
+
+    comments:any[]=[]
+
     blog: IBlog[] = [];
+
     id =Number( this.route.snapshot.paramMap.get('id'));
 
+    isLoogedin:boolean=false;
+
     ngOnInit(): void {
+
+        this.isLoogedin= sessionStorage.getItem('isLoggedIn') === 'true'
+        // console.log("status: ",this.isLoogedin)
 
         this.blogService.getBlogBYId(this.id).subscribe({
 
             next: data => {
 
                 this.blog = data
+                this.comments=data[0].comments
 
-                // console.log('blog/id call data: ', this.blog[0])
+                console.log('blog/id call data: ', data)
             },
             error: err => console.log('blog/id err: ', err)
         })
@@ -44,9 +59,9 @@ export class BlogPageComponent implements OnInit {
 
                 this.router.navigate(['/'])
 
-                alert("Your blog has deleted successfully")
+                // alert("Your blog has deleted successfully")
 
-                // console.log("delete blog call: ", data)
+                console.log("delete blog call: ", data)
             },
             error: err => console.log("err: ", err)
         })
@@ -54,7 +69,9 @@ export class BlogPageComponent implements OnInit {
 
     onSubmit():void{
         this.blogService.addComment(this.id,this.content).subscribe({
-            next:data=>console.log("comment call: ", data),
+            next:data=>{
+                console.log("comment call: ", data)
+            },
             error:err=>console.log("err comment :", err)
         })
     }
